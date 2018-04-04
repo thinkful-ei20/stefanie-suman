cb8fb0bee4b4f5177a74fc666c84a053da2a89a7
Suman Bandita <sumanbandita@gmail.com>
2018-04-04 11:56:52 -0400

    object drill 1 suman

--- object-suman.js
+++ object-suman.js
@@ -14,10 +14,10 @@ console.log(createMyObject());
 
 
 function updateObject(obj) {
-obj.foo = 'foo',
-obj.bar = 'bar',
-obj.bizz = 'bizz',
-obj.bang = 'bang'
+obj.foo = 'foo';
+obj.bar = 'bar';
+obj.bizz = 'bizz';
+obj.bang = 'bang';
 return obj;
 }
 

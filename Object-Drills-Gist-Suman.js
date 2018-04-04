af823647292203d660ba2b4c93604fdd20ce6ed5
Suman Bandita <sumanbandita@gmail.com>
2018-04-04 15:35:47 -0400

    fourth question

--- Object-Drills-Gist-Suman.js
+++ Object-Drills-Gist-Suman.js
@@ -25,3 +25,26 @@ const hobbitMeals ={
     meals : '"breakfast","second breakfast","elevenses","lunch","afternoon tea","dinner","supper"',
 }
 console.log(hobbitMeals['meals'].split(",")[3]);
+//============================================
+const objNameAndJob = [
+    {name : 'suman', jobTitle : 'Developer', boss : 'Bandita'},
+    {name : 'bandita', jobTitle : 'Architect', boss : 'Samantaray'},
+    {name : 'samantaray', jobTitle : 'QA', boss : 'Rajaram'},
+    {name : 'rajaram', jobTitle : 'Project Manager', boss : 'Nayak'},
+    {name : 'nayak', jobTitle : 'Team Lead'},
+]
+
+for(let i =0; i< objNameAndJob.length; i++){
+    console.log(objNameAndJob[i].name + "-- " + objNameAndJob[i].jobTitle);
+}
+
+// objNameAndJob.forEach(person => function(person){
+//     console.log(person.name + "-- "+ person.jobTitle);
+// });
+//======================================================
+for(let i =0; i< objNameAndJob.length; i++){
+    if(objNameAndJob[i].boss)
+        console.log(objNameAndJob[i].jobTitle + " " + objNameAndJob[i].name + " reports to " + objNameAndJob[i].boss);
+    else
+        console.log(objNameAndJob[i].jobTitle + " " + objNameAndJob[i].name + " doesn't reports to anybody. ");
+}

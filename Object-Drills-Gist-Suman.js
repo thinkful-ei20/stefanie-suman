cf60902097892b7b91b671dc768068b8d0454d68
Suman Bandita <sumanbandita@gmail.com>
2018-04-04 14:28:29 -0400

    getting started with object gist

---
+++ Object-Drills-Gist-Suman.js
@@ -0,0 +1,23 @@
+const loaf ={
+    flour : 300,
+    water : 210,
+    hydration :  function(){
+        let hydLevel = (this.water/this.flour)*100;
+        return hydLevel;
+    }
+}
+console.log(loaf.flour);
+console.log(loaf.water);
+console.log(loaf.hydration());
+//===========================================
+const myObj1= {
+    foo: 'Suman',
+    bar: 'Bandita',
+    fum: 'Samantaray',
+    quux: 'Rajaram',
+    spam:  'Nayak',
+}
+for(i in myObj1){
+    console.log(i,myObj1[i]);
+}
+//=========================================

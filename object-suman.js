efbb37369dce578317c6fd4e7475f112724034c5
Suman Bandita <sumanbandita@gmail.com>
2018-04-04 12:14:58 -0400

    object drill 1 complete

--- object-suman.js
+++ object-suman.js
@@ -58,3 +58,104 @@ return obj;
   console.log('SUCCESS: `updateObject` works correctly!');
   
 })();
+
+
+function personMaker() {
+    var person = {
+      firstName: 'Paul',
+      lastName: 'Jones',
+      // replace `null` with a function that uses self reference to return
+      // full name
+      fullName: function(){
+          return `${this.firstName} ${this.lastName}`;
+      },
+    };
+    return person;
+  }
+  
+  /* From here down, you are not expected to 
+     understand.... for now :)  
+     
+     
+     Nothing to see here!
+     
+  */
+  
+  (function testPersonMaker() {
+    var person = personMaker();
+    if (typeof person !== 'object') {
+      console.error('ERROR: `personMaker` must return an object');
+      return false;
+    }
+    if (typeof person.fullName !== 'function') {
+      console.error('ERROR: `fullName` must be a method');
+      return false;
+    }
+    if (person.fullName() !== 'Paul Jones') {
+      console.error(
+        'ERROR: The value for `fullName` should be "Paul Jones" but was ' +
+          person.fullName()
+      );
+      return false;
+    }
+    person.firstName = 'Lisa';
+    person.lastName = 'Simpson';
+    if (person.fullName() !== 'Lisa Simpson') {
+      console.error(
+        '`personMaker` is not using self reference correctly. ' +
+          'When firstName set to "Lisa" and lastName set to "Simpson", ' +
+          'should return "Lisa Simpson" but returned ' +
+          person.fullName()
+      );
+    }
+    console.log('SUCCESS: `updateObject` works correctly!');
+  })();
+  
+  function keyDeleter(obj) {
+    delete obj.foo;
+    delete obj.bar;
+    return obj;
+  }
+  
+  var sampleObj = {
+    foo: 'foo',
+    bar: 'bar',
+    bizz: 'bizz',
+    bang: 'bang',
+  };
+  
+  /* From here down, you are not expected to 
+     understand.... for now :)  
+     
+     
+     Nothing to see here!
+     
+  */
+  
+  (function testKeyDeleter() {
+    var obj = keyDeleter({
+      foo: 'foo',
+      bar: 'bar',
+      bizz: 'bizz',
+      bang: 'bang',
+    });
+  
+    if (typeof obj !== 'object') {
+      console.error('ERROR: `keyDeleter` must be return an object');
+      return false;
+    }
+    ['foo', 'bar'].forEach(function(key) {
+      if (key in obj) {
+        console.error('`keyDeleter` did not delete the key for ' + key);
+        return false;
+      }
+    });
+    ['bizz', 'bang'].forEach(function(key) {
+      if (!(key in obj && obj[key] === key)) {
+        console.error('`keyDeleter` is deleting keys other than `foo` and `bar`');
+        return false;
+      }
+    });
+    console.log('SUCCESS: `keyDeleter` works correctly!');
+  })();
+  
